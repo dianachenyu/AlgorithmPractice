03eb684164072483797ff8c72db5a3ebc2e2bc70
Diana Chenyu Zhang <dianachenyuzhang@gmail.com>
2020-12-06 15:54:41 -0800

    Create 1492. The kth Factor of n.py

---
+++ 1501-2000/1492. The kth Factor of n.py
@@ -0,0 +1,18 @@
+# Time O(sqrt(n))
+# Space O(1)
+
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        for i in range(1, int(sqrt(n))+1):
+            if n % i == 0:
+                k -= 1
+            if k == 0:
+                return i
+        for i in range(int(sqrt(n)), 0, -1):
+            if i * i == n:
+                continue
+            if n % i == 0:
+                k -= 1
+            if k == 0:
+                return n//i
+        return -1
