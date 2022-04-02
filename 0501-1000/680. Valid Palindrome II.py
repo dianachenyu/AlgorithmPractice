9e5dfb595a880c19941f354ffc1c6d66d3d98e9e
Diana Chenyu Zhang <dianachenyuzhang@gmail.com>
2022-04-01 22:13:22 -0700

    Create 680. Valid Palindrome II.py

---
+++ 0501-1000/680. Valid Palindrome II.py
@@ -0,0 +1,25 @@
+# Method 1
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def check(i, j, can_delete):
+            if i >= j:
+                return True
+            if s[i] == s[j]:
+                return check(i + 1, j - 1, can_delete)
+            elif can_delete:
+                return check(i + 1, j, False) or check(i, j - 1, False)
+            else:
+                return False
+        return check(0, len(s) - 1, True)
+
+      
+# Method 2      
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        i, j = 0, len(s) - 1
+        while s[i] == s[j]:
+            if i >= j:
+                return True
+            i += 1
+            j -= 1
+        return s[i + 1: j +1] == s[i + 1: j +1][::-1] or s[i: j] == s[i: j][::-1]
