b8c9d36d3624269d506f5cf1d0e936c649ef8293
Diana Zhang <dianachenyuzhang@gmail.com>
2023-03-26 10:22:46 -0700

    Create 2600. K Items With the Maximum Sum.py

---
+++ 2000-3000/2600. K Items With the Maximum Sum.py
@@ -0,0 +1,6 @@
+class Solution:
+    def kItemsWithMaximumSum(self, numOnes: int, numZeros: int, numNegOnes: int, k: int) -> int:
+        return min(k, numOnes) - max(0, k - numOnes - numZeros) 
+
+      
+# Brain Teaser 
