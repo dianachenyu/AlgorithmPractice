16cf85f3901c930fc1eadc4e17120a9f2d6e7dd0
Diana Zhang <dianachenyuzhang@gmail.com>
2024-05-19 12:04:29 -0700

    Update 31. Next Permutation.py

--- 0001-0500/31. Next Permutation.py
+++ 0001-0500/31. Next Permutation.py
@@ -3,25 +3,26 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        def reverse(nums, i, j):
+            while i < j:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j -= 1
+
         n = len(nums)
-        i = j = n - 1
-        while i - 1 >= 0 and nums[i - 1] >= nums[i]:
-            i -= 1
-        if i == 0: # nums are in descending order
-            nums.reverse()
-            return
-        
-        k = i - 1
-        while nums[j] <= nums[k]: # find the smallest num larger than nums[k]
-            j -= 1
-        nums[k], nums[j] = nums[j], nums[k]
-        
-        # reverse nums[k + 1:]
-        left, right = k + 1, n -1
-        while left < right:
-            nums[left], nums[right] = nums[right], nums[left]
-            left += 1
-            right -= 1
+        p = n - 2
+        while p >= 0 and nums[p] >= nums[p + 1]:
+            p -= 1
+        if p == -1:
+            return reverse(nums, 0, n - 1) 
+
+        q = n - 1
+        while q >= 0 and nums[q] <= nums[p]:
+            q -= 1
+
+        nums[p], nums[q] = nums[q], nums[p]
+        reverse(nums, p + 1, n - 1)
+
         
 # Time O(n)
 # Space O(1)
