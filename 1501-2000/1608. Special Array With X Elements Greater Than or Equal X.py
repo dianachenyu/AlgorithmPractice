c130b0b4337c3ab0d6d36f426404bc4857016ab9
Diana Zhang <dianachenyuzhang@gmail.com>
2024-05-26 23:20:59 -0700

    Create 1608. Special Array With X Elements Greater Than or Equal X.py

---
+++ 1501-2000/1608. Special Array With X Elements Greater Than or Equal X.py
@@ -0,0 +1,16 @@
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        counts = [0] * (n + 1)
+        for num in nums:
+            counts[min(n, num)] += 1
+        count = 0
+        for i in range(n, -1, -1):
+            count += counts[i]
+            if i == count:
+                return i
+        return -1
+
+      
+  # Time O(N)
+  # Space O(N)
